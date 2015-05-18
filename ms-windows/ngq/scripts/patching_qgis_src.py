2e703f7673663a22c1b07ea93bb802351ae59a35
alisovenko <alexander.lisovenko@gmail.com>
2015-05-18 09:44:00 +0000

    About pages customization

--- ms-windows/ngq/scripts/patching_qgis_src.py
+++ ms-windows/ngq/scripts/patching_qgis_src.py
@@ -48,24 +48,48 @@ def replaceFile(qgis_src_dir, settings_dir, configuration, config_key, ngq_src_d
             os.path.join(settings_dir, configuration[config_key]), 
             os.path.join(qgis_src_dir, ngq_src_dest) 
         )
+def putFile(qgis_src_dir, settings_dir, src, dest):
+    dest_dir_name = os.path.dirname(os.path.join(qgis_src_dir, dest))
+    if os.path.exists(dest_dir_name) == False:
+        os.mkdir(dest_dir_name)
+    shutil.copyfile(
+        os.path.join(settings_dir, src), 
+        os.path.join(qgis_src_dir, dest)
+    )
 
+def addTranslate(expression_en, expression_tr, context, dst_filename):
+    translation_dir = tempfile.mkdtemp('','ngq_translate_')
+    additional_tr_filename = os.path.join(translation_dir, 'additional.tr')
+    f = open(additional_tr_filename, 'w')
+    
+    f.write(
+        '''<TS version="2.0" language="ru">
+        <context>
+            <name>%s</name>
+            <message>
+                <source>%s</source>
+                <translation>%s</translation>
+            </message>
+        </context>
+        
+        </TS>'''%(context.encode('utf-8'), expression_en.encode('utf-8'), expression_tr.encode('utf-8'))
+    )
+    f.close()
+    
+    try:
+        res = subprocess.check_call(['lconvert', '-i', additional_tr_filename, '-i', dst_filename, '-o', dst_filename])
+        
+    except subprocess.CalledProcessError as ex:
+        sys.exit("ERROR! Merge ts files (lconvert): %s\n"%str(ex))
+    except:
+        sys.exit("ERROR!  Merge ts files (lconvert): Unexpected error: %s\n"%sys.exc_info()[0])
+    
+    shutil.rmtree(translation_dir)
+    
 def patchQGISsrc(qgis_src_dir, settings_dir, configuration):
     """
         Change icon & splash
     """
-    '''
-    if configuration.has_key(u'ngq_icon'):
-        shutil.copyfile(
-            os.path.join(settings_dir, configuration[u'ngq_icon']), 
-            os.path.join(qgis_src_dir, "src/app/qgis.ico") 
-        )
-    
-    if configuration.has_key(u'ngq_splash'):
-        shutil.copyfile(
-            os.path.join(settings_dir, configuration[u'ngq_splash']), 
-            os.path.join(qgis_src_dir, "images/splash/splash.png") 
-        )
-    '''
     replaceFile(qgis_src_dir, settings_dir, configuration, u'ngq_icon', "src/app/qgis.ico")
     replaceFile(qgis_src_dir, settings_dir, configuration, u'ngq_splash', "images/splash/splash.png")
     replaceFile(qgis_src_dir, settings_dir, configuration, u'ngq_icon_svg', "images/themes/default/mActionHelpAbout.svg")
@@ -92,4 +116,41 @@ def patchQGISsrc(qgis_src_dir, settings_dir, configuration):
         
         applyOnePatch(qgis_src_dir, patch_filename)
     
-    #shutil.rmtree(patch_dir)
+    """
+        Change qgis about
+    """
+    ts_dst_filename = os.path.join(qgis_src_dir, 'i18n', 'qgis_ru.ts')
+    if configuration.has_key(u'ngq_about_pages'):
+        #page_indexes = configuration[u'ngq_about_pages'].keys()
+        page_indexes = range(0, len(configuration[u'ngq_about_pages']))
+        #page_indexes.sort()
+        indexes4del =[]
+        indexes4add =[]
+        names4add =[]
+        files4add =[]
+        for index in page_indexes:
+            page_setting = configuration[u'ngq_about_pages'][index]
+            if page_setting[u'visible'] == False:
+                indexes4del.append(index)
+            if page_setting.has_key(u'name') and page_setting.has_key(u'content_file'):
+                indexes4add.append(index)
+                names4add.append(page_setting[u'name'][0])
+                files4add.append(page_setting[u'content_file'])
+                putFile(qgis_src_dir, settings_dir, u'about_pages_contents/%s'%page_setting[u'content_file'], "resources/about_pages_contents/%s"%page_setting[u'content_file'])
+                addTranslate(page_setting[u'name'][0], page_setting[u'name'][1], "QgsAbout", ts_dst_filename)
+        patch_template_filename = os.path.join( patches_templates_dir, "change_about_dialog.patch.template")
+        patch_filename = os.path.join(patch_dir, "change_about_dialog.patch")
+        
+        vars = {
+            u'LIST_INDEX_DEL': ",".join([str(v) for v in indexes4del]),
+            u'LIST_INDEX_ADD': ",".join([str(v) for v in indexes4add]),
+            u'LIST_NAME_ADD': ",".join(['"%s"'%v for v in names4add]),
+            u'LIST_CONTENT_FILENAME_ADD': ",".join(['"%s"'%v for v in files4add])
+        }
+        processPatchTemplate(vars, patch_template_filename, patch_filename)
+
+        if not os.path.exists(patch_filename):
+            sys.exit( "ERROR: Patch file (%s) not found"%patch_filename)
+        
+        applyOnePatch(qgis_src_dir, patch_filename)
+    shutil.rmtree(patch_dir)
