7e01716e045134e10a327800aa5e15d853f7da27
alisovenko <alexander.lisovenko@gmail.com>
2015-06-16 15:53:58 +0000

    moved build scripts to installer project

--- ms-windows/ngq/nsis/ngq-utils/install_font.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem %1 - font name; %2 - font filename
-set FONTS_FOLDER=%SYSTEMROOT%\fonts
-echo "Try set %2 (%1)"
-if NOT EXIST %FONTS_FOLDER%\%2 (COPY /Y %OSGEO4W_ROOT%\fonts\%2 %FONTS_FOLDER%)
-REG QUERY "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Fonts" /v %1
-if errorlevel 1 (
-     REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Fonts" /v %1 /t REG_SZ /d %2
-)
--- ms-windows/ngq/scripts/ngq-build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem %1 build directory
-rem %2 project file
-
-cd /d %1
-
-msbuild /p:Configuration=Release %2
-msbuild /p:Configuration=Release INSTALL.vcproj
