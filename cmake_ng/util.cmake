875390e895f8620530c167142b07c075ad7082e3
alisovenko <alexander.lisovenko@gmail.com>
2016-11-01 11:43:10 +0300

    Update borsch cmake scripts. Use gdal with enabled ecw driver.

--- cmake_ng/util.cmake
+++ cmake_ng/util.cmake
@@ -1,24 +1,110 @@
 ################################################################################
-# Project:  external projects
+# Project:  CMake4GDAL
 # Purpose:  CMake build scripts
 # Author:   Dmitry Baryshnikov, polimax@mail.ru
 ################################################################################
 # Copyright (C) 2015, NextGIS <info@nextgis.com>
-# Copyright (C) 2015 Dmitry Baryshnikov
+# Copyright (C) 2012,2013,2014 Dmitry Baryshnikov
 #
-# This script is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
 #
-# This script is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
 #
-# You should have received a copy of the GNU General Public License
-# along with this script.  If not, see <http://www.gnu.org/licenses/>.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
 ################################################################################
+
+
+function(check_version major minor rev)
+
+    # parse the version number from gdal_version.h and include in
+    # major, minor and rev parameters
+
+    file(READ ${CMAKE_CURRENT_SOURCE_DIR}/core/gcore/gdal_version.h GDAL_VERSION_H_CONTENTS)
+
+    string(REGEX MATCH "GDAL_VERSION_MAJOR[ \t]+([0-9]+)"
+      GDAL_MAJOR_VERSION ${GDAL_VERSION_H_CONTENTS})
+    string (REGEX MATCH "([0-9]+)"
+      GDAL_MAJOR_VERSION ${GDAL_MAJOR_VERSION})
+    string(REGEX MATCH "GDAL_VERSION_MINOR[ \t]+([0-9]+)"
+      GDAL_MINOR_VERSION ${GDAL_VERSION_H_CONTENTS})
+    string (REGEX MATCH "([0-9]+)"
+      GDAL_MINOR_VERSION ${GDAL_MINOR_VERSION})
+    string(REGEX MATCH "GDAL_VERSION_REV[ \t]+([0-9]+)"
+      GDAL_REV_VERSION ${GDAL_VERSION_H_CONTENTS})
+    string (REGEX MATCH "([0-9]+)"
+      GDAL_REV_VERSION ${GDAL_REV_VERSION})
+
+    set(${major} ${GDAL_MAJOR_VERSION} PARENT_SCOPE)
+    set(${minor} ${GDAL_MINOR_VERSION} PARENT_SCOPE)
+    set(${rev} ${GDAL_REV_VERSION} PARENT_SCOPE)
+
+endfunction(check_version)
+
+# search python module
+function(find_python_module module)
+    string(TOUPPER ${module} module_upper)
+    if(ARGC GREATER 1 AND ARGV1 STREQUAL "REQUIRED")
+        set(${module}_FIND_REQUIRED TRUE)
+    else()
+        if (ARGV1 STREQUAL "QUIET")
+            set(PY_${module}_FIND_QUIETLY TRUE)
+        endif()
+    endif()
+
+    if(NOT PY_${module_upper})
+        # A module's location is usually a directory, but for binary modules
+        # it's a .so file.
+        execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
+            "import re, ${module}; print(re.compile('/__init__.py.*').sub('',${module}.__file__))"
+            RESULT_VARIABLE _${module}_status
+            OUTPUT_VARIABLE _${module}_location
+            ERROR_QUIET
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+        if(NOT _${module}_status)
+            set(PY_${module_upper} ${_${module}_location} CACHE STRING
+                "Location of Python module ${module}")
+        endif(NOT _${module}_status)
+    endif(NOT PY_${module_upper})
+    find_package_handle_standard_args(PY_${module} DEFAULT_MSG PY_${module_upper})
+endfunction(find_python_module)
+
+function(set_libraries libs is_shared bld_dir release_name debug_name)
+    if (MSVC)
+        if(is_shared)
+            set(${libs}
+                    debug "${bld_dir}/Debug/${CMAKE_SHARED_LIBRARY_PREFIX}${debug_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+                    optimized "${bld_dir}/Release/${CMAKE_SHARED_LIBRARY_PREFIX}${release_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+                PARENT_SCOPE)
+        else()
+            set(${libs}
+                    debug"${bld_dir}/Debug/${CMAKE_STATIC_LIBRARY_PREFIX}${debug_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+                    optimized "${bld_dir}/Release/${CMAKE_STATIC_LIBRARY_PREFIX}${release_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+                PARENT_SCOPE)
+        endif()
+    else()
+        if(is_shared)
+            set(${libs}
+                "${bld_dir}/${CMAKE_SHARED_LIBRARY_PREFIX}${debug_name}${CMAKE_SHARED_LIBRARY_SUFFIX}"
+            PARENT_SCOPE)
+        else()
+            set(${libs}
+                "${bld_dir}/${CMAKE_STATIC_LIBRARY_PREFIX}${release_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+            PARENT_SCOPE)
+        endif()            
+    endif()    
+endfunction(set_libraries)
 
 function(report_version name ver)
 
@@ -28,7 +114,17 @@ function(report_version name ver)
         
     message(STATUS "${BoldYellow}${name} version ${ver}${ColourReset}")
     
-endfunction()  
+endfunction() 
+
+function(warning_msg text)
+    if(NOT SUPPRESS_VERBOSE_OUTPUT)
+    string(ASCII 27 Esc)
+    set(Red         "${Esc}[31m")
+    set(ColourReset "${Esc}[m")
+        
+    message(STATUS "${Red}${text}${ColourReset}")
+    endif()
+endfunction()    
 
 
 # macro to find packages on the host OS
@@ -79,31 +175,5 @@ macro( find_exthost_program )
         set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
     else()
         find_program( ${ARGN} )
-    endif()
-endmacro()
-
-
-# macro to find path on the host OS
-macro( find_exthost_path )
-    if(CMAKE_CROSSCOMPILING)
-        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
-        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER )
-        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER )
-        if( CMAKE_HOST_WIN32 )
-            SET( WIN32 1 )
-            SET( UNIX )
-        elseif( CMAKE_HOST_APPLE )
-            SET( APPLE 1 )
-            SET( UNIX )
-        endif()
-        find_path( ${ARGN} )
-        SET( WIN32 )
-        SET( APPLE )
-        SET( UNIX 1 )
-        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY )
-        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
-        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
-    else()
-        find_path( ${ARGN} )
     endif()
 endmacro()
